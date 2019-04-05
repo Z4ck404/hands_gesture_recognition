24177445fa177eed5e46ff29534d969bf178c3b3
Zakaria EL BAZI <za.elbazi@insea.ac.ma>
2019-04-05 11:22:07 +0200

    Create cnn.py

---
+++ cnn.py
@@ -0,0 +1,53 @@
+
+# Importing libraries
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+from keras.preprocessing import image
+
+classifier = Sequential()
+classifier.add(Conv2D(32, (3, 3), input_shape = (150, 150, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Flatten())
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 10, activation = 'softmax'))
+
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+#importing the images from dataset : 
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+training_set = train_datagen.flow_from_directory('hands_gesture_recognition/training_set',
+                                                 target_size = (150, 150),
+                                                 batch_size = 32,
+                                                 class_mode = 'categorical')
+test_set = test_datagen.flow_from_directory('hands_gesture_recognition/test_set',
+                                            target_size = (150, 150),
+                                            batch_size = 32,
+                                            class_mode = 'categorical')
+classifier.fit_generator(training_set,
+                         steps_per_epoch = 250,
+                         epochs = 25,
+                         validation_data = test_set,
+                         validation_steps = 200)
+'''
+# Part 3 - Making new predictions : 
+test_image = image.load_img('hands_gesture_recognition/cat_or_dog_1.jpg', target_size = (150, 150))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+    '''
